89c947d3a54aa92c0b3b5796fc5771fa88ceb628
RATIU5 <jcmemmott20@outlook.com>
2025-05-22 15:29:24 -0600

    edit: spaces won

--- packages/flowline/src/effects/logger.ts
+++ packages/flowline/src/effects/logger.ts
@@ -1,56 +1,56 @@
 import {
-	Context,
-	Effect,
-	FiberId,
-	FiberRef,
-	FiberRefs,
-	Logger,
-	Option,
+  Context,
+  Effect,
+  FiberId,
+  FiberRef,
+  FiberRefs,
+  Logger,
+  Option,
 } from "effect";
 
 export class LogOptions extends Context.Tag("LogOptions")<
-	LogOptions,
-	{
-		style: "basic" | "debug" | "none";
-	}
+  LogOptions,
+  {
+    style: "basic" | "debug" | "none";
+  }
 >() {}
 
 export const BunLogger = Effect.gen(function* () {
-	return Logger.make((_) => {
-		const currentStyle = FiberRefs.get(_.context, FiberRef.currentContext).pipe(
-			Option.flatMap((_) => Context.getOption(_, LogOptions)),
-			Option.map((_) => _.style),
-			Option.getOrElse(() => "none"),
-		);
-		const id = FiberId.threadName(_.fiberId);
-		switch (currentStyle) {
-			case "debug": {
-				const parts = [
-					`[${new Date(_.date).toISOString()}]`,
-					`[${_.logLevel.label}]`,
-					`[${id}]`,
-					_.message,
-				];
-				const structured = Object.entries(_.annotations)
-					.filter(([key]) => !key.startsWith("_") && key !== "style")
-					.map(([key, value]) => `${key}=${value}`)
-					.join(" ");
-				const logOutput = `${parts.join(" ")}${structured ? ` ${structured}` : ""}\n`;
-				Bun.stdout.write(logOutput);
-				break;
-			}
-			case "basic": {
-				const parts = [`[${_.logLevel.label}]`, _.message];
-				const structured = Object.entries(_.annotations)
-					.filter(([key]) => !key.startsWith("_") && key !== "style")
-					.map(([key, value]) => `${key}=${value}`)
-					.join(" ");
-				const logOutput = `${parts.join(" ")}${structured ? ` ${structured}` : ""}\n`;
-				Bun.stdout.write(logOutput);
-				break;
-			}
-			default:
-				Bun.stdout.write(String(_.message));
-		}
-	});
+  return Logger.make((_) => {
+    const currentStyle = FiberRefs.get(_.context, FiberRef.currentContext).pipe(
+      Option.flatMap((_) => Context.getOption(_, LogOptions)),
+      Option.map((_) => _.style),
+      Option.getOrElse(() => "none"),
+    );
+    const id = FiberId.threadName(_.fiberId);
+    switch (currentStyle) {
+      case "debug": {
+        const parts = [
+          `[${new Date(_.date).toISOString()}]`,
+          `[${_.logLevel.label}]`,
+          `[${id}]`,
+          _.message,
+        ];
+        const structured = Object.entries(_.annotations)
+          .filter(([key]) => !key.startsWith("_") && key !== "style")
+          .map(([key, value]) => `${key}=${value}`)
+          .join(" ");
+        const logOutput = `${parts.join(" ")}${structured ? ` ${structured}` : ""}\n`;
+        Bun.stdout.write(logOutput);
+        break;
+      }
+      case "basic": {
+        const parts = [`[${_.logLevel.label}]`, _.message];
+        const structured = Object.entries(_.annotations)
+          .filter(([key]) => !key.startsWith("_") && key !== "style")
+          .map(([key, value]) => `${key}=${value}`)
+          .join(" ");
+        const logOutput = `${parts.join(" ")}${structured ? ` ${structured}` : ""}\n`;
+        Bun.stdout.write(logOutput);
+        break;
+      }
+      default:
+        Bun.stdout.write(String(_.message));
+    }
+  });
 });
--- packages/flowline/src/index.ts
+++ packages/flowline/src/index.ts
@@ -4,21 +4,21 @@ import { Effect, Logger } from "effect";
 import { BunLogger, LogOptions } from "./effects/logger";
 
 const program = Effect.gen(function* () {
-	const mainApp = yield* app;
-	const server = Bun.serve({
-		port: 3000,
-		fetch: (req) => toWebHandler(mainApp)(req),
-	});
-	yield* Effect.logInfo(
-		`Server listening on http://localhost:${server.port}`,
-	).pipe(Effect.provideService(LogOptions, { style: "none" }));
+  const mainApp = yield* app;
+  const server = Bun.serve({
+    port: 3000,
+    fetch: (req) => toWebHandler(mainApp)(req),
+  });
+  yield* Effect.logInfo(
+    `Server listening on http://localhost:${server.port}`,
+  ).pipe(Effect.provideService(LogOptions, { style: "none" }));
 });
 
 const programWithLogger = Effect.gen(function* () {
-	const logger = yield* BunLogger;
-	return yield* program.pipe(
-		Effect.provide(Logger.replace(Logger.defaultLogger, logger)),
-	);
+  const logger = yield* BunLogger;
+  return yield* program.pipe(
+    Effect.provide(Logger.replace(Logger.defaultLogger, logger)),
+  );
 }).pipe(Effect.provideService(LogOptions, { style: "basic" }));
 
 Effect.runPromise(programWithLogger);
